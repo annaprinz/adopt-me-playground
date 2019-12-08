1dd5b4820c0cc89cf2e6ec6a736ec7df2221cc38
annaprinz <prinz.anna@gmail.com>
2019-12-08 16:06:33 +0100

    Converting Header to stateless functional component, with arrow func.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,18 @@
 import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>
-          Adoption
-          <span className="ofThe">
-            <span className="of">OF</span>
-          </span>
-          Dogs
-        </h1>
-        <h3 className="tagline">
-          <span>Adopt me - playground</span>
-        </h3>
-      </header>
-    );
-  }
-}
+//Converting Header to stateless functional component, with arrow func.
+const Header = props => (
+  <header className="top">
+    <h1>
+      Adoption
+      <span className="ofThe">
+        <span className="of">OF</span>
+      </span>
+      Dogs
+    </h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
 export default Header;
